aadf6a660bed2e09c18bb47f55f905adae67a5f7
Sebastian Aviles <savil030@fiu.edu>
2021-03-09 18:10:57 -0500

    Update sendMessage.py

--- sendMessage.py
+++ sendMessage.py
@@ -19,7 +19,7 @@ with open('listOfContacts.txt') as csv_file:
         ""
         ]
         # Edit message here
-        message = 'Brother {}, Status Report: Bruh I still need your transcript. Below is the link on how to submit it. It gives STEP by STEP instructions, if you have any problems just send me a text. {}'.format(row[1], row[2])
+        message = 'Brother {}, \nStatus Report: with midterms happening I want to make sure everyone is doing okay. If you could give me your grades using the link below that would be great. DW you dont need to download/upload anything. It should take only a few seconds. Its way easier than the last thing I asked you to do. If youre having any troubles in your classes please text me ASAP! so we can make sure you do well on your midterms {}'.format(row[1], row[2])
         file_path = os.path.realpath(__file__)
         print(file_path)
         file_path = file_path.split(os.path.basename(__file__))[0]
